18b7567b7cbace189643ecebbc8ff8d9f1c0f8b7
Dmitry G. Anderson <dandgerson@gmail.com>
2021-07-03 20:14:44 +0300

    description ▽
    1. character -> characterAtlas
    2. handle states immutable
    3. goup handlers
    4. add `moveTo` and `turnTo`
    5. add `animateSprite`
    6. drop legacy modules

--- src/index.js
+++ src/index.js
@@ -1,89 +1,12 @@
 import './styles/main.scss'
 
-import character from './assets/Male-3-Walk.png'
+import characterAtlas from './assets/Male-3-Walk.png' // 3 x 4
 
 const canvas = document.querySelector('#game')
 const { width: w, height: h } = canvas.getBoundingClientRect()
 const ctx = canvas.getContext('2d')
 
-const img = document.createElement('img')
-img.src = character
-
-const state = {}
-
-img.addEventListener('load', () => {
-  const directions = {
-    bottom: 0,
-    left: 1,
-    right: 2,
-    top: 3,
-  }
-
-  const shots = 3
-  let cycle = 0
-
-  const spriteW = 48
-  const spriteH = 48
-
-  let pY = h / 2 - spriteH / 2
-  let pX = w / 2 - spriteW / 2
-
-  const step = 10
-  let turned = 'bottom'
-
-  const triggerCycle = () => {
-    cycle = (cycle + 1) % shots
-  }
-
-  setInterval(() => {
-    switch (true) {
-      case state.isRightPressed: {
-        pX += pX + spriteW >= w ? 0 : step
-        turned = 'right'
-
-        triggerCycle()
-        break
-      }
-      case state.isLeftPressed: {
-        pX -= pX <= 0 ? 0 : step
-        turned = 'left'
-
-        triggerCycle()
-        break
-      }
-      case state.isUpPressed: {
-        pY -= pY <= 0 ? 0 : step
-        turned = 'top'
-
-        triggerCycle()
-        break
-      }
-      case state.isDownPressed: {
-        pY += pY + spriteH >= h ? 0 : step
-        turned = 'bottom'
-
-        triggerCycle()
-        break
-      }
-      default:
-        break
-    }
-
-    ctx.clearRect(0, 0, w, h)
-
-    ctx.drawImage(
-      img, // src
-      cycle * spriteW,
-      spriteH * directions[turned],
-      spriteW,
-      spriteH, // pos into image
-      pX,
-      pY,
-      spriteW,
-      spriteH, // pos into canvas
-    )
-  }, 100)
-})
+let keyState = {}
 
 const keyMap = {
   down: {
@@ -107,17 +30,159 @@ const keyMap = {
 const handleKeyDown = (e) => {
   Object.keys(keyMap).forEach((key) => {
     if (keyMap[key].keys.includes(e.key)) {
-      state[keyMap[key].state] = true
+      keyState = {
+        ...keyState,
+        [keyMap[key].state]: true,
+      }
     }
   })
 }
 const handleKeyUp = (e) => {
   Object.keys(keyMap).forEach((key) => {
     if (keyMap[key].keys.includes(e.key)) {
-      state[keyMap[key].state] = false
+      keyState = {
+        ...keyState,
+        [keyMap[key].state]: false,
+      }
     }
   })
 }
 
 document.addEventListener('keydown', handleKeyDown)
 document.addEventListener('keyup', handleKeyUp)
+
+const img = document.createElement('img')
+img.src = characterAtlas
+
+const handleImgLoad = () => {
+  const sprite = {
+    w: 48,
+    h: 48,
+    shots: 3,
+    step: 10,
+  }
+
+  let spriteState = {
+    cycle: 0,
+    posX: w / 2 - sprite.w / 2,
+    posY: h / 2 - sprite.h / 2,
+    turned: 'down',
+  }
+
+  const directionsMap = {
+    down: 0,
+    left: 1,
+    right: 2,
+    up: 3,
+  }
+
+  const animateSprite = () => {
+    spriteState = {
+      ...spriteState,
+      cycle: (spriteState.cycle + 1) % sprite.shots,
+    }
+  }
+
+  const moveTo = (direction) => {
+    const posMap = {
+      left: 'posX',
+      right: 'posX',
+      up: 'posY',
+      down: 'posY',
+    }
+
+    const moveToMap = {
+      left: () => spriteState.posX - (spriteState.posX <= 0 ? 0 : sprite.step),
+      right: () => spriteState.posX + (spriteState.posX + sprite.w >= w ? 0 : sprite.step),
+      up: () => spriteState.posY - (spriteState.posY <= 0 ? 0 : sprite.step),
+      down: () => spriteState.posY + (spriteState.posY + sprite.h >= h ? 0 : sprite.step),
+    }
+
+    spriteState = {
+      ...spriteState,
+      [posMap[direction]]: moveToMap[direction](),
+    }
+  }
+
+  const turnTo = (direction) => {
+    spriteState = {
+      ...spriteState,
+      turned: direction,
+    }
+  }
+
+  setInterval(() => {
+    switch (true) {
+      case keyState.isLeftPressed && keyState.isUpPressed: {
+        turnTo('left')
+        moveTo('left')
+        moveTo('up')
+        animateSprite()
+        break
+      }
+      case keyState.isRightPressed && keyState.isUpPressed: {
+        turnTo('right')
+        moveTo('right')
+        moveTo('up')
+        animateSprite()
+        break
+      }
+      case keyState.isRightPressed && keyState.isDownPressed: {
+        turnTo('right')
+        moveTo('right')
+        moveTo('down')
+        animateSprite()
+        break
+      }
+      case keyState.isLeftPressed && keyState.isDownPressed: {
+        turnTo('left')
+        moveTo('left')
+        moveTo('down')
+        animateSprite()
+        break
+      }
+      case keyState.isRightPressed: {
+        turnTo('right')
+        moveTo('right')
+        animateSprite()
+        break
+      }
+      case keyState.isLeftPressed: {
+        turnTo('left')
+        moveTo('left')
+        animateSprite()
+        break
+      }
+      case keyState.isUpPressed: {
+        turnTo('up')
+        moveTo('up')
+        animateSprite()
+        break
+      }
+      case keyState.isDownPressed: {
+        turnTo('down')
+        moveTo('down')
+        animateSprite()
+        break
+      }
+      default:
+        break
+    }
+
+    ctx.clearRect(0, 0, w, h)
+
+    ctx.drawImage(
+      img, // img elem
+      spriteState.cycle * sprite.w, // image pos x
+      sprite.h * directionsMap[spriteState.turned], // image pos y
+      sprite.w, // image width
+      sprite.h, // image height
+      spriteState.posX, // canvas pos x
+      spriteState.posY, // canvas pos y
+      sprite.w, // canvas image width
+      sprite.h, // canvas image height
+    )
+  }, 100)
+}
+
+img.addEventListener('load', handleImgLoad)
--- src/utils/clearCanvas.js
+++
@@ -1,9 +0,0 @@
-const clearCanvas = ({
-  ctx, h, w, frequency = 120,
-} = {}) => {
-  setInterval(() => {
-    ctx.clearRect(0, 0, h, w)
-  }, frequency)
-}
-
-export default clearCanvas
