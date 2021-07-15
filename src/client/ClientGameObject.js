324112e23aa9f813775fad2c4f5b1b3c31b7cc93
Dmitry G. Anderson <dandgerson@gmail.com>
2021-07-15 21:26:14 +0300

    add ClientCell & ClientGameObject & ClientInput

---
+++ src/client/ClientGameObject.js
@@ -0,0 +1,84 @@
+import MovableObject from 'common/MovableObject'
+
+class ClientGameObject extends MovableObject {
+  constructor(cfg) {
+    super()
+
+    const {
+      x, y, width, height,
+    } = cfg.cell
+
+    const { world } = cfg.cell
+    const gameObjs = world.game.gameObjects
+    const objCfg = { type: cfg.objCfg }
+
+    Object.assign(
+      this,
+      {
+        cfg,
+        x,
+        y,
+        width,
+        height,
+        spriteCfg: gameObjs[objCfg.type],
+        objectConfig: objCfg,
+        type: objCfg.type,
+        world,
+      },
+      cfg,
+    )
+  }
+
+  moveByCellCoord(dcol, drow, conditionCallback = null) {
+    const { cell } = this
+    this.moveToCellCoord(cell.cellCol + dcol, cell.cellRow + drow, conditionCallback)
+  }
+
+  moveToCellCoord(dcol, drow, conditionCallback = null) {
+    const { world } = this
+    const newCell = world.cellAt(dcol, drow)
+
+    if (!conditionCallback || conditionCallback(newCell)) this.setCell(newCell)
+  }
+
+  setCell(newCell) {
+    if (newCell) {
+      this.detouch()
+      this.cell = newCell
+      newCell.addGameObject(this)
+
+      const {
+        x, y, width, height,
+      } = newCell
+      Object.assign(this, {
+        x, y, width, height,
+      })
+    }
+  }
+
+  render(time) {
+    super.render(time)
+
+    const {
+      x, y, width, height, world,
+    } = this
+    const { engine } = world
+
+    const { sprite, frame, states } = this.spriteCfg
+
+    const spriteFrame = states ? states.main.frames[0] : frame
+
+    engine.renderSpriteFrame({
+      sprite, frame: spriteFrame, x, y, w: width, h: height,
+    })
+  }
+
+  detouch() {
+    if (this.cell) {
+      this.cell.removeGameObject(this)
+      this.cell = null
+    }
+  }
+}
+
+export default ClientGameObject
